fb55b6e6c67496373ba17dc9303abec614f4da65
Anton_Tsyrkunou <anton.azzot@yandex.ru>
2022-07-27 15:56:55 +0300

    01-base-commit

---
+++ src/main/java/com/tsyrkunou/jmpwep/JmpwepApplication.java
@@ -0,0 +1,13 @@
+package com.tsyrkunou.jmpwep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmpwepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmpwepApplication.class, args);
+	}
+
+}
